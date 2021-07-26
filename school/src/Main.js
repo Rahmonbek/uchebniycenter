31e66a5876483a33670d8943941be2413ec15173
Developer <62201299+Developer06092000@users.noreply.github.com>
2021-07-26 14:54:15 +0500

    Manguberdi
    Edit admin

--- school/src/Main.js
+++ school/src/Main.js
@@ -5,7 +5,7 @@ import styles from './top.module.css'
 import { Menu, Dropdown, Button } from 'antd';
 
 import {PieChartOutlined,} from '@ant-design/icons';
-import {FaBell,FaLanguage} from 'react-icons/fa'
+import {FaBell,FaLanguage, FaUserEdit} from 'react-icons/fa'
 import {DiJavascript1} from 'react-icons/di'
 import {BiGroup,BiDollarCircle,BiChevronDown} from 'react-icons/bi'
 import {BsPersonPlusFill} from 'react-icons/bs'
@@ -21,6 +21,7 @@ import Dashboard from './pages/Dashboard';
 import logo1 from './img/logo1.png'
 import Oquvchiqoshish from './pages/Oquvchiqoshish';
 import Tolov from './pages/Tolov'
+import Malumot_ozgartirish from './pages/Malumot_ozgartirish'
 import Oquvchi from './pages/Oquvchi';
 import Oqituvchi from './pages/Oqituvchi';
 import { Col, Row } from 'react-bootstrap';
@@ -77,116 +78,122 @@ state = {
     const { SubMenu } = Menu;
     return (
       <div >
-      <BrowserRouter>
-      <div className={styles.top}>
-          <h1 className={this.state.collapsed? styles.header1:styles.header2}>IT Tower</h1>
-          <div onClick={this.toggleCollapsed} className={this.state.collapsed? styles.btntop1:styles.btntop}>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
-          <div className={this.state.collapsed?styles.search1:styles.search}>
-            <input className={this.state.open? styles.inputSearch1:styles.inputSearch}  type="text" placeholder="Qidirish ..."/>
-            <div className={this.state.open? styles.iconSearch1:styles.iconSearch}>
-            <GoSearch onClick={this.open} style={{fontSize:'19px',color:"#3F6AD8"}}/>
+        <BrowserRouter>
+        <div className={styles.top}>
+            <h1 className={this.state.collapsed? styles.header1:styles.header2}>IT Tower</h1>
+            <div onClick={this.toggleCollapsed} className={this.state.collapsed? styles.btntop1:styles.btntop}>
+              <div></div>
+              <div></div>
+              <div></div>
             </div>
-            <div onClick={this.close} className={this.state.open? styles.closeIcon1:styles.closeIcon}>
-               <VscChromeClose style={{fontSize:'22px',color:"#8AA4E6"}}/>
+            <div className={this.state.collapsed?styles.search1:styles.search}>
+              <input className={this.state.open? styles.inputSearch1:styles.inputSearch}  type="text" placeholder="Qidirish ..."/>
+              <div className={this.state.open? styles.iconSearch1:styles.iconSearch}>
+              <GoSearch onClick={this.open} style={{fontSize:'19px',color:"#3F6AD8"}}/>
+              </div>
+              <div onClick={this.close} className={this.state.open? styles.closeIcon1:styles.closeIcon}>
+                <VscChromeClose style={{fontSize:'22px',color:"#8AA4E6"}}/>
+              </div>
             </div>
-          </div>
-          <div className={styles.bell}>
-          <FaBell style={{fontSize:'18px',color:'#D92550'}}/>
-          </div>
-          <div className={styles.language}>
-          <FaLanguage style={{fontSize:'23px',color:'#3F6AD8'}}/>
-          </div>
-          <span className={styles.hr}>|</span>
-          <div className={styles.user}>
-          
-            <Row>
-              <Col className={styles.logo1} lg={3}> <img src={logo1} style={{width:'45px', height:'45px'}}></img></Col>
-              <Col className={styles.logo1text} lg={9}>
-               
-    <h6>Ismoilov Rahmon</h6>
-                <p>IT Tower</p>
-              </Col>
-            </Row>
+            <div className={styles.bell}>
+            <FaBell style={{fontSize:'18px',color:'#D92550'}}/>
             </div>
-      </div> 
-     <div style={{display:'flex',flexDirection:'row'}}>
-     <div style={{backgroundColor:'rgba(255,255,255,0.8)'}} className={this.state.collapsed? styles.collapsed1:styles.collapsed2}>
-        <Menu
-        className={styles.menuNav}
-          mode="inline"
-          style={{ boxShadow: '0 0.46875rem 2.1875rem rgb(4 9 20 / 3%), 0 0.9375rem 1.40625rem rgb(4 9 20 / 3%), 0 0.25rem 0.53125rem rgb(4 9 20 / 5%), 0 0.125rem 0.1875rem rgb(4 9 20 / 3%)'}}
-          inlineCollapsed={this.state.collapsed}
-        > 
-        <h5 className={this.state.collapsed? styles.headingMenu1:styles.headingMenu2}>Admin</h5>
-        <div className={this.state.collapsed? styles.hrMenu1:styles.hrMenu2}></div>
-          <Menu.Item key="1" icon={<PieChartOutlined style={{fontSize:'18px',color:'rgba(0,0,0,0.3)'}}/>} className={this.state.collapsed? styles.list1:styles.list2} > 
-          <Link style={{textDecoration:'none',color:'rgba(0,0,0,0.7)'}} to="/cabinet/"> Bosh bo'lim</Link>
-          </Menu.Item>
-          <Menu.Item key="2" icon={<DiJavascript1 style={{fontSize:'20px',color:'rgba(0,0,0,0.3)'}}/>} className={this.state.collapsed? styles.list1:styles.list2}>
-          <Link style={{textDecoration:'none',color:'rgba(0,0,0,0.7)'}} to="/cabinet/kurslar">Kurslar</Link>
-          </Menu.Item>
-          <SubMenu key="sub1" icon={<BiGroup style={{fontSize:'20px',color:'rgba(0,0,0,0.3)'}}/>} className={this.state.collapsed? styles.list1:styles.list2} title="Guruhlar">
-            <Menu.Item className={this.state.collapsed? styles.list1:styles.list2} key="4">Front-end (React)</Menu.Item>
-            <Menu.Item className={this.state.collapsed? styles.list1:styles.list2} key="5">Python+Jango</Menu.Item>
-            <Menu.Item className={this.state.collapsed? styles.list1:styles.list2} key="6">Unit</Menu.Item>
-          </SubMenu>
-          <Menu.Item key="8" icon={<TiTickOutline style={{fontSize:'20px',color:'rgba(0,0,0,0.3)'}}/>} className={this.state.collapsed? styles.list1:styles.list2}>
-           <Link style={{textDecoration:'none',color:'rgba(0,0,0,0.7)'}} to="/cabinet/davomat">Davomat</Link>
-          </Menu.Item>
-          <Menu.Item key="9" icon={<BiDollarCircle style={{fontSize:'20px',color:'rgba(0,0,0,0.3)'}}/>} className={this.state.collapsed? styles.list1:styles.list2}>
-          <Link style={{textDecoration:'none',color:'rgba(0,0,0,0.7)'}} to="/cabinet/tolov">To'lov</Link>
-          </Menu.Item>
-          <h5 className={this.state.collapsed? styles.headingMenu1:styles.headingMenu2}>Registratsiya</h5>
-        <div className={this.state.collapsed? styles.hrMenu1:styles.hrMenu2}></div>
-          <Menu.Item key="10" icon={<BsPersonPlusFill style={{fontSize:'20px',color:'rgba(0,0,0,0.3)'}}/>} className={this.state.collapsed? styles.list1:styles.list2}>
-          <Link style={{textDecoration:'none',color:'rgba(0,0,0,0.7)'}} to="/cabinet/oquvchi">O'quvchi qo'shish</Link>
-          </Menu.Item>
-          <Menu.Item key="11" icon={<AiOutlineGooglePlus style={{fontSize:'20px',color:'rgba(0,0,0,0.3)'}}/>} className={this.state.collapsed? styles.list1:styles.list2}>
-          <Link style={{textDecoration:'none',color:'rgba(0,0,0,0.7)'}} to="/cabinet/guruhlar">Guruh qo'shish</Link>
-          </Menu.Item>
-          <Menu.Item key="11" className={this.state.collapsed? styles.list1:styles.list2}>
-          <Link style={{textDecoration:'none',color:'rgba(0,0,0,0.7)'}} to="/cabinet/lcenter">Ro'yxatdan o'tish</Link>
-          </Menu.Item>
-          <Menu.Item key="12" icon={<ImUserPlus style={{fontSize:'20px',color:'rgba(0,0,0,0.3)'}}/>} className={this.state.collapsed? styles.list1:styles.list2}>
-          <Link style={{textDecoration:'none',color:'rgba(0,0,0,0.7)'}} to="/cabinet/oqituvchi">O'qituvchi qo'shish</Link>
-          </Menu.Item>
-        </Menu>
-      </div>  
-     <div className={this.state.collapsed?styles.forget1:styles.forget}>
-     <Switch>
-     <Route exact path="/cabinet/davomat">
-         <Davomat/>
-       </Route>
-       <Route exact path="/cabinet">
-         <Dashboard/>
-       </Route>
-       <Route exact path="/cabinet/guruhlar">
-         <Guruhlar/>
-       </Route>
-       <Route exact path="/cabinet/lcenter">
-         <LearningCenter/>
-       </Route>
-       <Route exact path="/cabinet/oquvchi">
-         <Oquvchi/>
-       </Route>
-       <Route exact path="/cabinet/tolov">
-         <Tolov/>
-       </Route>
-       <Route exact path="/cabinet/oqituvchi">
-         <Oqituvchi/>
-       </Route>
-       <Route exact path="/cabinet/kurslar">
-         <Malumot/>
-       </Route>
+            <div className={styles.language}>
+            <FaLanguage style={{fontSize:'23px',color:'#3F6AD8'}}/>
+            </div>
+            <span className={styles.hr}>|</span>
+            <div className={styles.user}>
+            
+              <Row>
+                <Col className={styles.logo1} lg={3}> <img src={logo1} style={{width:'45px', height:'45px'}}></img></Col>
+                <Col className={styles.logo1text} lg={9}>
+                
+      <h6>Ismoilov Rahmon</h6>
+                  <p>IT Tower</p>
+                </Col>
+              </Row>
+              </div>
+        </div> 
+      <div style={{display:'flex',flexDirection:'row'}}>
+      <div style={{backgroundColor:'rgba(255,255,255,0.8)'}} className={this.state.collapsed? styles.collapsed1:styles.collapsed2}>
+          <Menu
+          className={styles.menuNav}
+            mode="inline"
+            style={{ boxShadow: '0 0.46875rem 2.1875rem rgb(4 9 20 / 3%), 0 0.9375rem 1.40625rem rgb(4 9 20 / 3%), 0 0.25rem 0.53125rem rgb(4 9 20 / 5%), 0 0.125rem 0.1875rem rgb(4 9 20 / 3%)'}}
+            inlineCollapsed={this.state.collapsed}
+          > 
+          <h5 className={this.state.collapsed? styles.headingMenu1:styles.headingMenu2}>Admin</h5>
+          <div className={this.state.collapsed? styles.hrMenu1:styles.hrMenu2}></div>
+            <Menu.Item key="1" icon={<PieChartOutlined style={{fontSize:'18px',color:'rgba(0,0,0,0.3)'}}/>} className={this.state.collapsed? styles.list1:styles.list2} > 
+            <Link style={{textDecoration:'none',color:'rgba(0,0,0,0.7)'}} to="/cabinet/"> Bosh bo'lim</Link>
+            </Menu.Item>
+            <Menu.Item key="2" icon={<DiJavascript1 style={{fontSize:'20px',color:'rgba(0,0,0,0.3)'}}/>} className={this.state.collapsed? styles.list1:styles.list2}>
+            <Link style={{textDecoration:'none',color:'rgba(0,0,0,0.7)'}} to="/cabinet/kurslar">Kurslar</Link>
+            </Menu.Item>
+            <SubMenu key="sub1" icon={<BiGroup style={{fontSize:'20px',color:'rgba(0,0,0,0.3)'}}/>} className={this.state.collapsed? styles.list1:styles.list2} title="Guruhlar">
+              <Menu.Item className={this.state.collapsed? styles.list1:styles.list2} key="4">Front-end (React)</Menu.Item>
+              <Menu.Item className={this.state.collapsed? styles.list1:styles.list2} key="5">Python+Jango</Menu.Item>
+              <Menu.Item className={this.state.collapsed? styles.list1:styles.list2} key="6">Unit</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="8" icon={<TiTickOutline style={{fontSize:'20px',color:'rgba(0,0,0,0.3)'}}/>} className={this.state.collapsed? styles.list1:styles.list2}>
+            <Link style={{textDecoration:'none',color:'rgba(0,0,0,0.7)'}} to="/cabinet/davomat">Davomat</Link>
+            </Menu.Item>
+            <Menu.Item key="9" icon={<BiDollarCircle style={{fontSize:'20px',color:'rgba(0,0,0,0.3)'}}/>} className={this.state.collapsed? styles.list1:styles.list2}>
+            <Link style={{textDecoration:'none',color:'rgba(0,0,0,0.7)'}} to="/cabinet/tolov">To'lov</Link>
+            </Menu.Item>
+            <Menu.Item key="3" icon={<FaUserEdit style={{fontSize:'20px',color:'rgba(0,0,0,0.3)'}}/>} className={this.state.collapsed? styles.list1:styles.list2}>
+            <Link style={{textDecoration:'none',color:'rgba(0,0,0,0.7)'}} to="/cabinet/malumot_ozgartirish">Ma'lumotlarni o'zgartirish</Link>
+            </Menu.Item>
+            <h5 className={this.state.collapsed? styles.headingMenu1:styles.headingMenu2}>Registratsiya</h5>
+          <div className={this.state.collapsed? styles.hrMenu1:styles.hrMenu2}></div>
+            <Menu.Item key="10" icon={<BsPersonPlusFill style={{fontSize:'20px',color:'rgba(0,0,0,0.3)'}}/>} className={this.state.collapsed? styles.list1:styles.list2}>
+            <Link style={{textDecoration:'none',color:'rgba(0,0,0,0.7)'}} to="/cabinet/oquvchi">O'quvchi qo'shish</Link>
+            </Menu.Item>
+            <Menu.Item key="11" icon={<AiOutlineGooglePlus style={{fontSize:'20px',color:'rgba(0,0,0,0.3)'}}/>} className={this.state.collapsed? styles.list1:styles.list2}>
+            <Link style={{textDecoration:'none',color:'rgba(0,0,0,0.7)'}} to="/cabinet/guruhlar">Guruh qo'shish</Link>
+            </Menu.Item>
+            <Menu.Item key="11" className={this.state.collapsed? styles.list1:styles.list2}>
+            <Link style={{textDecoration:'none',color:'rgba(0,0,0,0.7)'}} to="/cabinet/lcenter">Ro'yxatdan o'tish</Link>
+            </Menu.Item>
+            <Menu.Item key="12" icon={<ImUserPlus style={{fontSize:'20px',color:'rgba(0,0,0,0.3)'}}/>} className={this.state.collapsed? styles.list1:styles.list2}>
+            <Link style={{textDecoration:'none',color:'rgba(0,0,0,0.7)'}} to="/cabinet/oqituvchi">O'qituvchi qo'shish</Link>
+            </Menu.Item>
+          </Menu>
+        </div>  
+      <div className={this.state.collapsed?styles.forget1:styles.forget}>
+      <Switch>
+        <Route exact path="/cabinet/davomat">
+          <Davomat/>
+        </Route>
+        <Route exact path="/cabinet">
+          <Dashboard/>
+        </Route>
+        <Route exact path="/cabinet/guruhlar">
+          <Guruhlar/>
+        </Route>
+        <Route exact path="/cabinet/malumot_ozgartirish">
+          <Malumot_ozgartirish/>
+        </Route>
+        <Route exact path="/cabinet/lcenter">
+          <LearningCenter/>
+        </Route>
+        <Route exact path="/cabinet/oquvchi">
+          <Oquvchi/>
+        </Route>
+        <Route exact path="/cabinet/tolov">
+          <Tolov/>
+        </Route>
+        <Route exact path="/cabinet/oqituvchi">
+          <Oqituvchi/>
+        </Route>
+        <Route exact path="/cabinet/kurslar">
+          <Malumot/>
+        </Route>
 
-     </Switch>
-     </div>
-     </div>
-        </BrowserRouter>
+      </Switch>
+      </div>
+      </div>
+          </BrowserRouter>
       </div>
     )
   }
---
+++ school/src/pages/Malumot_ozgartirish.js
@@ -0,0 +1,165 @@
+import {  Button,  Form, Input } from 'antd';
+import TextArea from 'antd/lib/input/TextArea';
+import React, {useState} from 'react'
+import { Row } from 'react-bootstrap';
+import './Malumot_ozgartirish.css';
+const url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnPEMT1O67rK1vm-LfTOAU28Xkwdb2Vx4Ekw&usqp=CAU'
+
+export default function Malumot_ozgartirish() {
+    
+    const [admin, setAdmin]=useState([
+        {
+            name: 'IT Tower',
+            email: 'info@gmail.com',
+            number: 998999349707,
+            tgLink: 'https://t.me/IT_Tower_Admin',
+            insLink: 'https://t.me/IT_Tower_Admin',
+            faLink: 'https://t.me/IT_Tower_Admin',
+            address: 'Manzil',
+            izoh: 'Izoh...',
+        }
+    ])
+    const tailFormItemLayout = {
+    wrapperCol: {
+        xs: {
+        span: 24,
+        offset: 0,
+        },
+        sm: {
+        span: 16,
+        offset: 8,
+        },
+    },
+    };
+    return (
+        <div>
+            <Form
+            name="edit"
+            style={{marginLeft:"auto", marginRight:"auto", marginTop:"30px", marginBottom: "30px", padding:"40px", }}
+            >
+                <Row>
+                    <div className="col-lg-6 offset-md-3 col-md-9 px-3 m-0">
+                        <Form.Item
+                            name="name"
+                            label="O'quv markaz nomi"
+                        >
+                            <Input 
+                                className="input1"
+                                placeholder="IT Tower"
+                                defaultValue={admin[0].name}
+                            />
+                        </Form.Item>
+                        <Form.Item
+                            name="email"
+                            label="E-mail"
+                            rules={[
+                            {
+                                type: 'email',
+                                message: 'The input is not valid E-mail!',
+                            },
+                            ]}
+                        >
+                            <Input 
+                                className="input1"
+                                placeholder="info@gmail.com"
+                                defaultValue={admin[0].email}
+                            />
+                        </Form.Item>
+                        <Form.Item
+                            name="number"
+                            label="Tel. raqam"
+                            >
+                            <Input 
+                                className="input1"
+                                placeholder="998*********"
+                                defaultValue={admin[0].number}
+                            />
+                        </Form.Item>
+                        <Form.Item
+                            name="tgLink"
+                            label="Telegram link"
+                            >
+                            <Input 
+                                className="input1"
+                                placeholder="https://t.me/IT_Tower_Admin"
+                                defaultValue={admin[0].tgLink}
+                            />
+                        </Form.Item>
+                    </div>
+                    <div className="col-lg-6 offset-md-3 col-md-9 px-3 m-0">
+                        <Form.Item
+                            name="file"
+                            label="O`quv markaz logotipi"
+                            >
+                            <Input 
+                                className="input1"
+                                type="file"
+                            />
+                        </Form.Item>
+                        <div className="w-100 h-100" style={{position: 'relative'}}>
+                            <img src={url} className="images" style={{width:"150px", height:"150px"}} alt="Logotip rasmi"/>
+                        </div>
+                    </div>
+                    <Form.Item
+                        name="insLink"
+                        label="Instagram link"
+                        className="col-lg-6 offset-md-3 col-md-9 px-3"
+                        >
+                        <Input 
+                            className="input1"
+                            placeholder="https://t.me/IT_Tower_Admin"
+                            defaultValue={admin[0].insLink}
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        name="faLink"
+                        label="Facebook link"
+                        className="col-lg-6 offset-md-3 col-md-9 px-3"
+                        >
+                        <Input 
+                            className="input1"
+                            placeholder="https://t.me/IT_Tower_Admin"
+                            defaultValue={admin[0].faLink}
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        name="address"
+                        label="Manzil"
+                        className="col-lg-6 offset-md-3 col-md-9 px-3"
+                        >
+                        <TextArea 
+                            className="input1"
+                            placeholder="Manzil"
+                            style={{height: 150}}
+                            defaultValue={admin[0].address}
+                        >
+                        </TextArea>
+                    </Form.Item>
+                    <Form.Item
+                        name="izoh"
+                        label="Izoh"
+                        className="col-lg-6 offset-md-3 col-md-9 px-3"
+                        >
+                        <TextArea 
+                            className="input1"
+                            placeholder="Izoh..."
+                            style={{height: 150}}
+                            defaultValue={admin[0].izoh}
+                        >
+                        </TextArea>
+                    </Form.Item>
+
+                </Row>
+
+
+
+
+            <Form.Item {...tailFormItemLayout}>
+                <Button type="primary" htmlType="submit">
+                Save
+                </Button>
+            </Form.Item>
+            </Form>
+        </div>
+    )
+}
