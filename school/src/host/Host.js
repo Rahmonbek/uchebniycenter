c936ab7c358a34a1473b8ecf4cf2864284662363
Rahmonbek <83601261+Rahmonbek@users.noreply.github.com>
2021-08-01 21:07:28 +0500

    vsd

--- school/src/host/Host.js
+++ school/src/host/Host.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 export default url="https://ews-backend.herokuapp.com/"
-export id="1"
+export const id="1"
 export default httpsReaquest=(config)=>{
     return(
         axios({
